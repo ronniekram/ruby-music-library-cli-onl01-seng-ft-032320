1f1f796583dba016afdce13b5e075459ddc02e00
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-09 03:02:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -57,7 +57,7 @@ class MusicLibraryController
     #prints all songs by a particular artist in numbered, alphabetized list
     if artist = Artist.find_by_name(input)
       artist.sort {|a, b| a.name <=> b.name}.each_with_index do |s, i|
-        puts "#{i + 1}. #{s.name}"
+        puts "#{i + 1}. #{s.name} - #{s.genre.name}"
       end
     end
   end

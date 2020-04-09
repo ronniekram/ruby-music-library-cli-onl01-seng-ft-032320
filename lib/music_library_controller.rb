5e229a291b24cde3cad94f2e0daa089d1c634e83
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-09 02:06:22 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,94 @@
+require 'pry'
+class MusicLibraryController
+  attr_accessor :path 
+  
+  def initialize(path = './db/mp3s')
+    @path = path
+    importer = MusicImporter.new(path)
+    importer.import
+  end
+  
+  def call
+    input = '' 
+    if input != exit
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+      input = gets.strip.downcase
+    end
+  end
+  
+  def list_songs
+    #not hard coded 
+    #prints all songs in numbered, alphabetical list
+        #binding.pry
+        Song.all.sort_by{|s| s.name}.each_with_index do |s, i|
+          puts "#{i + 1}. #{s.artist.name} - #{s.name} - #{s.genre.name}" 
+        end
+  end
+
+  def list_artists
+    #not hard coded 
+    #prints all artist in numbered, alphabetical list
+    artists = Artist.all.sort_by{|a| a.name}
+    
+    artists.each_with_index {|s, i| puts "#{i + 1}. #{s.name}"} 
+  end
+
+  def list_genres
+    #not hard coded 
+    #prints all genres in numbered, alphabetical list
+    genres = Genre.all.sort_by{|g| g.name}
+    
+    genres.each_with_index {|s, i| puts "#{i + 1}. #{s.name}"} 
+  end
+
+  def list_songs_by_artist
+    #prompts user to enter an artist
+    puts "Please enter the name of an artist:"
+    #accepts user input
+    input = gets.strip.downcase
+    #prints all songs by a particular artist in numbered, alphabetized list
+    if artist = Artist.find_by_name(input)
+      artist.sort_by{|a| a.name}.each_with_index do |s, i|
+        puts "#{i + 1}. #{s.name}"
+      end
+    end
+  end
+
+  def list_songs_by_genre
+    #prompts user to enter an artist
+    puts "Please enter the name of a genre:"
+    #accepts user input
+    input = gets.strip.downcase
+    #prints all songs by a particular artist in numbered, alphabetized list
+    if genre = Genre.find_by_name(input)
+      genre.sort_by{|g| g.name}.each_with_index do |s, i|
+        puts "#{i + 1}. #{s.name}"
+      end
+    end
+  end
+
+  def play_song
+    #prompts user to choose a song from alphabetized list output from lists_songs
+    list_songs
+    puts "Which song number would you like to play?"
+      #accepts user input
+    input = gets.strip
+      #if valid input, "play" the matching song from the list output
+    length = list_songs.length + 1
+    if input(1, length)
+      input -= 1
+      puts "Playing #{s.name[input]} by #{s.artist.name[input]}"
+    end 
+    #checks that the user entered a number between 1 and total number of songs in library
+    #does nothing if no match found
+end
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,65 @@
+require 'pry'
+class Song
+  attr_accessor :name
+  attr_reader :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end 
+  
+  def self.find_by_name(name)
+    all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+  
+  def self.new_from_filename(filename)
+    #binding.pry
+    artist_name, song_name, genre_name = filename.split(" - ")
+    genre_name = genre_name.gsub('.mp3', '')
+    
+    song = self.find_or_create_by_name(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = Genre.find_or_create_by_name(genre_name)
+    song
+  end
+
+  def self.create_from_filename(filename)
+    @@all << new_from_filename(filename)
+  end 
+  
+end 

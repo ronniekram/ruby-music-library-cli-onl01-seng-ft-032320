d2404cd3f27ac4a5edbc40db1123abbb10b07f48
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-09 03:07:35 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,7 +27,7 @@ class MusicLibraryController
   def list_songs
     #not hard coded 
     #prints all songs in numbered, alphabetical list
-        #binding.pry
+        binding.pry
         Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
           puts "#{i + 1}. #{s.artist.name} - #{s.name} - #{s.genre.name}" 
         end
